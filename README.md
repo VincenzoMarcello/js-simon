9520ae979cb1ae7e2ddc925c0e043a6436a3bb48
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-08-24 18:57:24 +0200

    feat: add ex complete

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina
 
 ## Svolgimento
 
-- CREO UN OGGETTO CON LA DATA DI DOMANI
-
-- CREO UN OGGETTO CON LA DATA DI OGGI
-
-- PRENDO I MILLISECONDI PASSATI PER ARRIVARE A DOMANI
-
-- PRENDO I MILLISECONDI PASSATI PER ARRIVARE A OGGI
-
-- VEDO I MILLISECONDI RESTANTI TRA OGGI E DOMANI
+- faccio un oggetto date che mi da i ms di oggi dal 1970
+- faccio un oggetto date che mi da i ms di domani a una certa ora dal 1970
+- faccio la differenza tra i ms di domani e quelli di oggi e li trasformo in s
+- mi ricavo secondi,minuti,ore,giorni
+- mi prendo gli elementi nel DOM e li uso per stamparci i dati del temopo che abbiamo trovato
+- aggiungo un controllo per aggiungere lo 0
+- mi creo una funzione che avvia il countdown
+- metto tutto in una timing function per avviare il countdown
+- faccio un controllo che blocca il countdown e una volta arrivato a 0
--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
-//* CREO UN OGGETTO CON LA DATA DI DOMANI
-const tomorrow = new Date("2023-08-24 9:30");
-//* CREO UN OGGETTO CON LA DATA DI OGGI
-const today = new Date();
+//! VARIABILI GLOBALI
+//* RECUPERO GLI ELEMENTI DAL DOM CON L'ID
+const secondsEl = document.getElementById("seconds");
+const minutesEl = document.getElementById("minutes");
+const hoursEl = document.getElementById("hours");
+const daysEl = document.getElementById("days");
 
-//* PRENDO I MILLISECONDI PASSATI PER ARRIVARE A DOMANI
-const tomorrowSecond = tomorrow.getTime();
-//* PRENDO I MILLISECONDI PASSATI PER ARRIVARE A OGGI
-const tomorrowToday = today.getTime();
+const interval = setInterval(countdown, 1000);
+countdown();
 
-//* VEDO I MILLISECONDI RESTANTI TRA OGGI E DOMANI
-const restTime = tomorrowSecond - tomorrowToday;
-console.log(restTime);
+// ! FUNZIONE CHE INIZIALIZZA IL COUNTDOWN
+function countdown() {
+  //* CREO UN OGGETTO CON LA DATA DI OGGI
+  const now = new Date();
+  //* CREO UN OGGETTO CON LA DATA DI DOMANI
+  const tomorrow = new Date("2023-08-25 18:46");
 
-//* CALCOLIAMO I SECONDI I MINUTI E LE ORE
-let seconds = Math.floor(restTime / 1000) % 60;
-let minutes = Math.floor(restTime / (60 * 1000)) % 60;
-let hours = Math.floor(restTime / (60 * 60 * 1000)) % 60;
+  //* PRENDO I MILLISECONDI PASSATI PER ARRIVARE A OGGI
+  const msNow = now.getTime();
+  //* PRENDO I MILLISECONDI PASSATI PER ARRIVARE A DOMANI
+  const msTomorrow = tomorrow.getTime();
 
-const secondElement = document.getElementById("seconds");
+  //* VEDO I MILLISECONDI RESTANTI TRA OGGI E DOMANI E LI TRASFORMO IN SECONDI
+  let restTime = Math.floor((msTomorrow - msNow) / 1000);
 
-setInterval(function () {
-  seconds--;
-
-  if (seconds < 0) {
-    seconds = 60;
-    seconds--;
-  }
-  if (seconds < 10) seconds = "0" + seconds;
-
-  secondElement.innerHTML = seconds;
-  console.log(hours, minutes, seconds);
-}, 1000);
-
-setInterval(function () {
-  minutes--;
-
-  if (minutes < 0) {
-    minutes = 60;
-    minutes--;
+  //* METTIAMO UN CONTROLLO PER BLOCCARE IL COUNTDOWN UNA VOLTA ARRIVATO A 0
+  if (restTime <= 0) {
+    clearInterval(interval);
+    restTime = 0;
   }
 
-  console.log(hours, minutes, seconds);
-}, 60000);
+  //* CALCOLIAMO I SECONDI I MINUTI E LE ORE
+  let seconds = restTime % 60;
+  let minutes = Math.floor((restTime / 60) % 60);
+  let hours = Math.floor((restTime / 60 / 60) % 24);
+  let days = Math.floor(restTime / 60 / 60 / 24);
+
+  //* STAMPO NEL DOM E USO OPERATORE TERNARIO PER AGGIUNGERE LO 0
+  //* QUANDO I NUMERI SONO INFERIORI DI 10
+  secondsEl.innerText = seconds < 10 ? "0" + seconds : seconds;
+  minutesEl.innerText = minutes < 10 ? "0" + minutes : minutes;
+  hoursEl.innerText = hours < 10 ? "0" + hours : hours;
+  daysEl.innerText = days < 10 ? "0" + days : days;
+}
